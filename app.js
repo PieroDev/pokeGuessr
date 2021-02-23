c4845992072be41fe79e9229798a7963848085d8
Piero Zuniga <pierolh12@gmail.com>
2021-02-23 01:31:26 -0300

    Implementando opciones

--- app.js
+++ app.js
@@ -16,48 +16,53 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
-    /* var idOpciones = [];
-    var pokeOpciones = [];
-    var listaPokes = [];
-    var idCorrecta = idOpciones[ Math.floor((Math.random()*4)+1)] ;
-    var pokeCorrecto = pokeOpciones[ Math.floor((Math.random()*4)+1)] ;
 
-    nuevasOpciones();
-    impOpciones();
-    res.render("game", {pokeId: idCorrecta, pokeName: pokeCorrecto});
-
-
-    
-
-    function nuevasOpciones(){
-        for(i=0; i>4; i++){
-            var pokeId = Math.floor((Math.random()*151)+1); 
-            var poke = P.getPokemonByName(pokeId);
-            idOpciones.push(pokeId);
-            pokeOpciones.push(poke.name);
-            console.log(poke.name);
-        }
-    }
-
-    function impOpciones(){
-        console.log(pokeOpciones);
-    } */
-
+    var pokeList =[];
     var pokeId = Math.floor((Math.random()*151)+1);
     
     P.getPokemonByName(pokeId)
     .then(function(pokemonTittle){
         pokeName = pokemonTittle.name;
-   
+        console.log("Hola1");
     })
     .catch(function(error){
         console.log("There was an ERROR", error);
+        console.log("Hola2");
     })
     .then(function(){
-
+        console.log("Hola3");
         res.render("game", {pokeId: pokeId, pokeName: pokeName});
     })
+    .then(function(){
+        var pokeOpciones = [];
+        var pokeOpcionesId = [];
 
+        for(var i=0; i<4; i++){
+            var pokeId = Math.floor((Math.random()*151)+1);
+            P.getPokemonByName(pokeId)
+            .then(function(pokemonTittle){
+                pokeName = pokemonTittle.name;
+                pokeOpciones.push(pokeName);
+            })
+            .then(function(){
+                console.log(pokeOpciones);
+                return pokeOpciones;
+            })
+        }
+        
+        
+        
+    })
+    
+    /* var pokeOpciones = [];
+
+    for(var i=0; i<4; i++){
+        var pokeId = Math.floor((Math.random()*151)+1);
+        var poke = P.getPokemonByName(pokeId);
+        console.log(poke.name);
+    } */
+
+    
 
 
 
