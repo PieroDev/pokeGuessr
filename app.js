3749cf095d2831f757fc6ea790286d1132ea9833
Piero Zuniga <pierolh12@gmail.com>
2021-02-23 00:57:54 -0300

    Se agrego archivo bootstrap y cambios menores

--- app.js
+++ app.js
@@ -16,13 +16,39 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
+    /* var idOpciones = [];
+    var pokeOpciones = [];
+    var listaPokes = [];
+    var idCorrecta = idOpciones[ Math.floor((Math.random()*4)+1)] ;
+    var pokeCorrecto = pokeOpciones[ Math.floor((Math.random()*4)+1)] ;
 
-    var pokeId = Math.floor((Math.random()*151)+1); 
+    nuevasOpciones();
+    impOpciones();
+    res.render("game", {pokeId: idCorrecta, pokeName: pokeCorrecto});
 
+
+    
+
+    function nuevasOpciones(){
+        for(i=0; i>4; i++){
+            var pokeId = Math.floor((Math.random()*151)+1); 
+            var poke = P.getPokemonByName(pokeId);
+            idOpciones.push(pokeId);
+            pokeOpciones.push(poke.name);
+            console.log(poke.name);
+        }
+    }
+
+    function impOpciones(){
+        console.log(pokeOpciones);
+    } */
+
+    var pokeId = Math.floor((Math.random()*151)+1);
+    
     P.getPokemonByName(pokeId)
     .then(function(pokemonTittle){
         pokeName = pokemonTittle.name;
-        console.log(pokeName);
+   
     })
     .catch(function(error){
         console.log("There was an ERROR", error);
